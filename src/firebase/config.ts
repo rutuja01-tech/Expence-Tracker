af975cb93b93c84718d4b63152ba352b15b59ea1
Rutuja Kshirsagar <rutujakshirsagar001@gmail.com>
2026-02-15 13:48:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7876239128-41981",
+  "appId": "1:232196478642:web:487368b884ea4f939adbab",
+  "apiKey": "AIzaSyD07XMQYuv4-PLTCIsLEra_E-gMFxQbKbI",
+  "authDomain": "studio-7876239128-41981.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "232196478642"
+};
